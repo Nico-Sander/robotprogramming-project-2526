f0484107d6402c0adb55d70a5f54f34d51d29d33
Nico-Sander <sandernico512@gmail.com>
2025-12-16 13:46:08 +0100

    basic setup

---
+++ src/optimize_path.py
@@ -0,0 +1,7 @@
+class OptimizeFlyby():
+    def __init__(self):
+        pass
+
+    def optimizePath(path: list = [], planner = None, config: dict = {}):
+        """Returns name of node and the Überschleifwert (r) belonging to it."""
+        pass
